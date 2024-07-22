25dac2df332be6dd7d042cb07d12f3bc4b859bfd
SimLabJW <89232609+SimLabJW@users.noreply.github.com>
2024-07-22 18:00:53 +0900

    robomaster ip done, camera, sensor comm need

---
+++ RobotController.py
@@ -0,0 +1,109 @@
+import base64
+import cv2
+import datetime
+import random
+import json
+
+from robomaster import robot, camera, conn
+
+class RobotController():
+    def __init__(self):
+        self.ep_robot = robot.Robot()
+
+        self.distance = 0  # 초기 거리 값
+
+        self.ep_robot = robot.Robot()
+        self.ip_to_sn = {
+        "192.168.50.31": "3JKCK2S00305WL",
+        "192.168.50.221": "3JKCK6U0030A6U",
+        "192.168.50.39": "3JKCK980030EKR"
+        }
+        
+    def Research_Device(self):
+        self.ip_list = conn.scan_robot_ip_list(timeout=5)
+        if self.ip_list:
+            selected_ips = self.select_robot_ips(self.ip_list)
+            robots = [{"name": f"robot_{self.ip_to_sn[ip][-4:]}", "sn": self.ip_to_sn[ip]} for ip in selected_ips if ip in self.ip_to_sn]
+            
+            a = self.Device_Connect(robots)
+            return a
+        else:
+            print("No robots found.")
+
+    def select_robot_ips(self, ip_list):
+
+        return [ip_list[i] for i in range(len(ip_list))]
+
+    def Device_Connect(self, robots):
+        if robots:
+            return robots
+        else:
+            return print("No valid robots selected.")
+
+     ################################################       
+
+    def Device_Camera(self, sn):
+        
+        self.ep_robot.initialize(conn_type="sta", sn=sn)
+        self.ep_robot.camera.start_video_stream(display=False, resolution=camera.STREAM_360P)
+        return self.ep_robot
+
+    def Device_Sensor(self, controller):
+        # 로봇의 센서와 아머 이벤트 콜백을 설정
+        self.ep_robot.armor.set_hit_sensitivity(comp="all", sensitivity=100)
+        self.ep_robot.armor.sub_hit_event(controller.hit_callback)
+        self.ep_robot.sensor.sub_distance(freq=20, callback=controller.tof_callback)
+
+    ##################################################
+
+    async def shutdown(self):
+        # 로봇과의 연결을 종료
+        self.ep_robot_camera.camera.stop_video_stream()
+        self.ep_robot_camera.close()
+        self.comm.close()
+
+    @staticmethod
+    def time():
+        # 현재 시간을 문자열 형식으로 반환
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+
+    def tof_callback(self, sub_info):
+        # ToF 센서로부터 거리를 업데이트
+        self.distance = sub_info[0]
+
+    def hit_callback(self, sub_info):
+        # 로봇이 충격을 받을 때 호출
+        armor_id, hit_type = sub_info
+        timestamp = self.time()
+        img = self.ep_robot_camera.camera.read_cv2_image(strategy="newest")
+        image_data = base64.b64encode(cv2.imencode('.jpg', img)[1]).decode('utf-8') if img is not None else None
+        hit_info = [armor_id, hit_type]
+        
+        # 충격시 로봇 색 변환
+        print(f"{self.robot_name} Physical hit event: armor_id={armor_id}, hit_type={hit_type}")
+        self.ep_robot.led.set_led(comp="all", r=random.randint(0, 255), g=random.randint(0, 255), b=random.randint(0, 255))  
+        self.comm.send(self.robot_name, timestamp, image_data, self.distance, hit_info)
+
+
+
+    def json_convert(id, timestamp, image_data, distance, hit_info=None):
+        # 데이터를 서버로 전송
+        hit_info = hit_info if hit_info else [None, None]
+
+        packet = {
+            "id": id,
+            "time": timestamp,
+            "imageData": image_data,
+            "distance": distance,
+            "hitInfo": hit_info
+        }
+
+        json_data = json.dumps(packet) + '\n'
+
+        return json_data.encode('utf-8')
+
+    @staticmethod
+    def time():
+        # 현재 시간을 문자열 형식으로 반환
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
--- serve.py
+++ serve.py
@@ -3,72 +3,47 @@ import json
 import base64
 from datetime import datetime
 from threading import Thread
+from RobotController import *
+
 
 class Server:
-    def __init__(self, address="0.0.0.0", port=11014):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((address, port))
-        self.socket.listen(5)
-        print("Waiting for client connections...")
+    def __init__(self):
+        self.robotcontroller = RobotController()
+        self.unity_socket()
 
-    def start(self):
-        while True:
-            conn, addr = self.socket.accept()
-            print(f"Connected to {addr}")
-            client_thread = Thread(target=self.handle_client, args=(conn, addr))
-            client_thread.start()
+    def unity_socket(self):
+        # for unity
+        self.socket_unity = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_unity.bind(("0.0.0.0", 11013)) # Unity Port 11013
+        self.socket_unity.listen(5)
+        self.conn_u, self.addr_u = self.socket_unity.accept()
+        print(f"Connected to {self.addr_u}")
+        print("Waiting for unity connections...")
 
-    def handle_client(self, conn, addr):
-        buffer = ""
-        data_list = []
-        try:
-            while True:
-                data = conn.recv(4096).decode('utf-8')
-                if not data:
-                    print(f"Connection closed by {addr}")
-                    break
-                buffer += data
-                while '\n' in buffer:
-                    packet, buffer = buffer.split('\n', 1)
-                    print(f"Received packet: {packet}")
-                    data_list.append(json.loads(packet))
-        except ConnectionResetError:
-            print(f"Connection reset by {addr}")
-        except Exception as e:
-            print(f"Error handling client {addr}: {e}")
-        finally:
-            if data_list:
-                self.save_data(data_list)
-            conn.close()
+    # def device_socket(self):
+    #     # for unity
+    #     self.socket_device = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     self.socket_device.bind(("0.0.0.0", 11014)) # Unity Port 11013
+    #     self.socket_device.listen(5)
+    #     self.conn_d, self.addr_d = self.socket_device.accept()
+    #     print(f"Connected to {self.addr_d}")
+    #     print("Waiting for device connections...")
 
-    def save_data(self, data_list):
-        try:
-            id = data_list[0]["id"]
-            start_timestamp = data_list[0]["time"].replace(":", "-").replace(" ", "_")
-            end_timestamp = data_list[-1]["time"].replace(":", "-").replace(" ", "_")
+    def start(self): # Unity Open Recv 
 
-            # Save image data and replace it with the filename in the JSON data
-            for packet in data_list:
-                if packet["imageData"]:
-                    image_bytes = base64.b64decode(packet["imageData"])
-                    safe_timestamp = packet["time"].replace(":", "-").replace(" ", "_")
-                    image_filename = f"{id}_{safe_timestamp}.jpg"
-                    with open(image_filename, 'wb') as image_file:
-                        image_file.write(image_bytes)
-                    print(f"Image saved as {image_filename}")
-                    packet["imageData"] = image_filename
+        recv_data = self.conn_u.recv(4096).decode("utf-8")
 
-            # Save all other data to a single JSON file
-            json_filename = f"{id}_{start_timestamp}_to_{end_timestamp}.json"
-            with open(json_filename, 'w') as json_file:
-                json.dump(data_list, json_file, indent=4)
-            print(f"Data saved as {json_filename}")
+        if recv_data == "Unity Start":
+            print(recv_data)
+            a = self.robotcontroller.Research_Device()
+            print(type(a))
+            print(a)
+            self.close()
 
-        except Exception as e:
-            print(f"Error saving data: {e}")
 
     def close(self):
-        self.socket.close()
+        self.socket_unity.close()
+        # self.socket_device.close()
 
 if __name__ == '__main__':
     server = Server()
@@ -78,3 +53,66 @@ if __name__ == '__main__':
         print("Server shutting down...")
     finally:
         server.close()
+
+
+
+
+
+
+
+    # def handle_client(self, conn, addr):
+    #     buffer = ""
+    #     data_list = []
+    #     try:
+    #         while True:
+    #             data = conn.recv(4096).decode('utf-8')
+    #             if not data:
+    #                 print(f"Connection closed by {addr}")
+    #                 break
+    #             buffer += data
+    #             while '\n' in buffer:
+    #                 packet, buffer = buffer.split('\n', 1)
+    #                 print(f"Received packet: {packet}")
+    #                 data_list.append(json.loads(packet))
+    #     except ConnectionResetError:
+    #         print(f"Connection reset by {addr}")
+    #     except Exception as e:
+    #         print(f"Error handling client {addr}: {e}")
+    #     finally:
+    #         conn.close()
+
+
+
+    # def start(self): # Unity Open Recv 
+        
+    #     while True:
+    #         conn, addr = self.socket.accept()
+    #         print(f"Connected to {addr}")
+    #         client_thread = Thread(target=self.handle_client, args=(conn, addr))
+    #         client_thread.start()
+
+# def save_data(self, data_list):
+#         try:
+#             id = data_list[0]["id"]
+#             start_timestamp = data_list[0]["time"].replace(":", "-").replace(" ", "_")
+#             end_timestamp = data_list[-1]["time"].replace(":", "-").replace(" ", "_")
+
+#             # Save image data and replace it with the filename in the JSON data
+#             for packet in data_list:
+#                 if packet["imageData"]:
+#                     image_bytes = base64.b64decode(packet["imageData"])
+#                     safe_timestamp = packet["time"].replace(":", "-").replace(" ", "_")
+#                     image_filename = f"{id}_{safe_timestamp}.jpg"
+#                     with open(image_filename, 'wb') as image_file:
+#                         image_file.write(image_bytes)
+#                     print(f"Image saved as {image_filename}")
+#                     packet["imageData"] = image_filename
+
+#             # Save all other data to a single JSON file
+#             json_filename = f"{id}_{start_timestamp}_to_{end_timestamp}.json"
+#             with open(json_filename, 'w') as json_file:
+#                 json.dump(data_list, json_file, indent=4)
+#             print(f"Data saved as {json_filename}")
+
+#         except Exception as e:
+#             print(f"Error saving data: {e}")
